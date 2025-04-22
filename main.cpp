1fe0761c7371cd218d6e2cbaa5503be293766fdd
Angie Lorena Pineda Morales <127700100+Broflovsky580@users.noreply.github.com>
2025-04-22 11:28:05 -0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,38 @@
 #include <iostream>
+#include <chrono>
+#include <fstream>
 #include "Particula.h"
 
-void simular(const std::string& nombreSimulacion, const Vector2D& campo, const Particula& particulaInicial, int pasos, double dt) {
-    std::cout << "\n--- Simulación: " << nombreSimulacion << " ---\n";
-    Particula p = particulaInicial;
+void simular(const std::string& nombreArchivo, const Vector2D& campo, const Particula& inicial, int pasos, double dt) {
+    std::ofstream archivo(nombreArchivo);
+    archivo << "Paso,Posicion_X,Posicion_Y,Velocidad_X,Velocidad_Y\n";
 
+    Particula p = inicial;
     for (int i = 0; i < pasos; ++i) {
+        archivo << i << "," << p.posicion.x << "," << p.posicion.y << ","
+                << p.velocidad.x << "," << p.velocidad.y << "\n";
         p.actualizar(campo, dt);
-        std::cout << "Paso " << i << ": " << p << "\n";
     }
+
+    archivo.close();
+    std::cout << "Simulación guardada en " << nombreArchivo << "\n";
 }
 
 int main() {
     double dt = 0.01;
-    int pasos = 100;
+    int pasos = 1000;
 
     // Simulación 1
     Vector2D campo1(6.0, -1.0);
     Particula particula1(Vector2D(0, 0), Vector2D(0, 0), 5.0, 21.0);
-    simular("Partícula 1 - Campo (5, -3)", campo1, particula1, pasos, dt);
+    simular("simulacion_1.csv", campo1, particula1, pasos, dt);
 
     // Simulación 2
     Vector2D campo2(27.0, 2.0);
     Particula particula2(Vector2D(3, 9), Vector2D(9, 3), 2.0, -1.0);
-    simular("Partícula 2 - Campo (-2, 4)", campo2, particula2, pasos, dt);
+    simular("simulacion_2.csv", campo2, particula2, pasos, dt);
 
     return 0;
 }
+
+
